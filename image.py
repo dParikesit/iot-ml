25a1e5564d839d7566aef234f9bcb53cb9ea95d5
Kyle <kyle.treoxer@gmail.com>
2024-05-11 14:34:28 +0700

    fix: uploaded image name

--- image.py
+++ image.py
@@ -21,28 +21,40 @@ def process_image(data):
 
     print(f"Analyzing {file_name}.")
 
-    __upload_image(file_name, blob)
-
-
-def __upload_image(file_name, blob: storage.Blob):
-    """Uploads the image to the Cloud Storage bucket.
-
-    Args:
-        file_name: name of the file
-        blob: blob data
-    """
     # Download the image from the Cloud Storage bucket.
     _, temp_local_filename = tempfile.mkstemp()
     blob.download_to_filename(temp_local_filename)
-    print(f"Image {file_name} was downloaded to {temp_local_filename}.")
+
+    # temp_local_filename is the file path to the temporary file.
+    # Process the image from the temporary file.
+
+    # with open(temp_local_filename, "rb") as image_file:
+    #     image = image_file.read()
+
+    # Save the image to a new file
+
+    # with open(temp_local_filename, "wb") as image_file:
+    #     image_file.write(image)
+
+    # Upload the processed image to the Cloud Storage bucket.
+    __upload_image(file_name, temp_local_filename)
+
+
+def __upload_image(blob_name, file_name):
+    """Uploads the image to the Cloud Storage bucket.
+
+    Args:
+        blob_name: name of the blob in the bucket
+        file_name: name of the file in local storage (tempfile)
+    """
 
     upload_bucket_name = os.getenv("UPLOAD_BUCKET_NAME")
     upload_bucket = storage_client.bucket(upload_bucket_name)
 
     # Upload the image to the Cloud Storage bucket.
-    upload_blob = upload_bucket.blob(file_name)
-    upload_blob.upload_from_filename(temp_local_filename)
-    print(f"Image {file_name} was uploaded to {upload_bucket_name}.")
+    upload_blob = upload_bucket.blob(blob_name)
+    upload_blob.upload_from_filename(file_name)
+    print(f"Image {blob_name} was uploaded to {upload_bucket_name}.")
 
     # Delete the temporary file.
-    os.remove(temp_local_filename)
+    os.remove(file_name)
